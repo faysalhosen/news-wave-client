5f8948e8c2d16efcac72dd09ca145de491bdd0ab
faysalhosen <faysanhosen7025@gmail.com>
2023-12-01 20:40:34 +0600

    subscriptions page added

---
+++ src/pages/Subscriptions/Subscriptions.jsx
@@ -0,0 +1,200 @@
+
+import { Link } from "react-router-dom";
+
+const Subscriptions = () => {
+    return (
+        <div>
+            
+            <section className="text-gray-600 body-font overflow-hidden">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-col text-center w-full mb-10">
+                        <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Explore Our Monthly Subscriptions</h1>
+                        
+
+                    </div>
+                    <div className="flex flex-wrap -m-4">
+                        <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+                            <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+                                <h2 className="text-sm tracking-widest title-font mb-1 font-medium">STARTER</h2>
+                                <h1 className="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">Free</h1>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Unlimited access for free articles
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Monthly Newsletter
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-6">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>No Customer Support
+                                </p>
+                                <Link to='/login' className="flex items-center mt-auto text-white bg-gunblack border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500 rounded">Just Login & Browse
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </Link>
+                                
+                            </div>
+                        </div>
+                        <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+                            <div className="h-full p-6 rounded-lg border-2 border-gunblack flex flex-col relative overflow-hidden">
+                                <span className="bg-gunblack text-white px-3 py-1 tracking-widest text-xs absolute right-0 top-0 rounded-bl">POPULAR</span>
+                                <h2 className="text-sm tracking-widest title-font mb-1 font-medium">PRO</h2>
+                                <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+                                    <span>$15</span>
+                                    <span className="text-lg ml-1 font-normal text-gray-500">/mo</span>
+                                </h1>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Starter Package Services
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Exclusive content
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Ad-free browsing
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-6">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Live Chat, Email Support
+                                </p>
+                                <Link to='/payment' className="flex items-center mt-auto text-white bg-gunblack border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500  rounded">Buy Subscription
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </Link>
+                                
+                            </div>
+                        </div>
+                        <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+                            <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+                                <h2 className="text-sm tracking-widest title-font mb-1 font-medium">BUSINESS</h2>
+                                <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+                                    <span>$25</span>
+                                    <span className="text-lg ml-1 font-normal text-gray-500">/mo</span>
+                                </h1>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Pro package services
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Access to premium archives
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Customizable news feed
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Live Chat (24/7),Phone Support
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-6">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Ticket System for issue resolution
+                                </p>
+                                <Link to='/payment' className="flex items-center mt-auto text-white bg-gunblack border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500  rounded">Buy Subscription
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </Link>
+                                
+                            </div>
+                        </div>
+                        <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+                            <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
+                                <h2 className="text-sm tracking-widest title-font mb-1 font-medium">ENTERPRISE</h2>
+                                <h1 className="text-5xl text-gray-900 leading-none flex items-center pb-4 mb-4 border-b border-gray-200">
+                                    <span>$35</span>
+                                    <span className="text-lg ml-1 font-normal text-gray-500">/mo</span>
+                                </h1>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Business package services
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>White-label option
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Dedicated account manager
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-2">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Extended phone support hours
+                                </p>
+                                <p className="flex items-center text-gray-600 mb-6">
+                                    <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
+                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
+                                            <path d="M20 6L9 17l-5-5"></path>
+                                        </svg>
+                                    </span>Social Media Support
+                                </p>
+                                <Link to='/payment' className="flex items-center mt-auto text-white bg-gunblack border-0 py-2 px-4 w-full focus:outline-none hover:bg-gray-500  rounded">Buy Subscription
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-auto" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </Link>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Subscriptions;
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,120 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Homepage from "../pages/Homepage/Homepage";
+import AllArticles from "../pages/AllArticles/AllArticles";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Subscriptions from "../pages/Subscriptions/Subscriptions";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Add from "../pages/Add/Add";
+import PrivateRoute from "./PrivateRoute";
+import Dashboard from "../dashboard/dashboard/Dashboard";
+import ArticleDetails from "../pages/AllArticles/ArticleDetails";
+import MyArticles from "../pages/MyArticles/MyArticles";
+import ManageArticles from "../dashboard/ManageArticles/ManageArticles";
+import Update from "../dashboard/Update/Update";
+import ManageAllArticles from "../dashboard/ManageArticles/ManageAllArticles";
+import PostApproval from "../pages/PostApproval/PostApproval";
+import ManageAllUsers from "../dashboard/ManageAllUsers/ManageAllUsers";
+import DashboardHome from "../pages/DashboardHome/DashboardHome";
+import AuthorRequest from "../pages/AuthorRequest/AuthorRequest";
+import AuthorApproval from "../dashboard/AuthorApproval/AuthorApproval";
+import Payment from "../pages/Payment/Payment";
+import Premium from "../pages/Premium/Premium";
+
+export const routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <App></App>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: "/",
+                element: <Homepage></Homepage>
+            },
+            {
+                path: "/all",
+                element: <AllArticles></AllArticles>
+            },
+            {
+                path: "/article/:id",
+                element: <PrivateRoute><ArticleDetails></ArticleDetails></PrivateRoute>,
+                loader: ({params})=>fetch(`http://localhost:3000/post/${params.id}`)
+            },
+            {
+                path: "/subscriptions",
+                element: <Subscriptions></Subscriptions>
+            },
+            {
+                path: "/login",
+                element: <Login></Login>
+            },
+            {
+                path: "/register",
+                element: <Register></Register>
+            },
+            {
+                path: "/add",
+                element: <PrivateRoute><Add></Add></PrivateRoute>
+            },
+            {
+                path: "/myarticles",
+                element: <MyArticles></MyArticles>,
+
+            },
+            {
+                path: "/authorrequest",
+                element:<AuthorRequest></AuthorRequest>
+            },
+            {
+                path: "/payment",
+                element: <Payment></Payment>
+            },
+            {
+                path: "/premium",
+                element: <Premium></Premium>
+            }
+            
+        ]
+    },
+    {
+        path: "/dashboard",
+        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
+        children:[
+            {
+                path: "/dashboard",
+                element: <DashboardHome></DashboardHome>
+            },
+            {
+                path: "/dashboard/manage",
+                element: <ManageArticles></ManageArticles>,
+            },
+            {
+                path: "/dashboard/update/:id",
+                element: <Update></Update>,
+                loader: ({params})=>fetch(`http://localhost:3000/post/${params.id}`)
+            },
+            {
+                path: "/dashboard/manageall",
+                element: <ManageAllArticles></ManageAllArticles>
+            },
+            {
+                path: "/dashboard/postapproval",
+                element: <PostApproval></PostApproval>
+            },
+            {
+                path: "/dashboard/manageusers",
+                element: <ManageAllUsers></ManageAllUsers>
+            },
+            {
+                path: "/dashboard/add",
+                element: <Add></Add>,
+            },
+            {
+                path: "/dashboard/authorrequest",
+                element: <AuthorApproval></AuthorApproval>
+            }
+            
+        ]
+    }
+])
