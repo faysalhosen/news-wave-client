21ac7d0501f959db9ab6687d7678a04f3ddc5e4f
faysalhosen <faysanhosen7025@gmail.com>
2023-12-01 20:42:26 +0600

    update home page

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,29 @@
+import { Helmet } from "react-helmet-async";
+import Footer from "../../shared/Footer/Footer";
+import Headlines from "../../shared/Headlines/Headlines";
+import Hero from "../../shared/Hero/Hero";
+import Features from "../Features/Features";
+import Subscriptions from "../Subscriptions/Subscriptions";
+import AllArticlesHome from "../AllArticles/AllArticlesHome";
+
+
+const Homepage = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>News Wave | Home</title>
+            </Helmet>
+            
+            <Hero></Hero>
+            <Headlines></Headlines>
+            <AllArticlesHome></AllArticlesHome>
+            <Subscriptions></Subscriptions>
+            <Features></Features>
+            
+            <Footer></Footer>
+            
+        </div>
+    );
+};
+
+export default Homepage;
