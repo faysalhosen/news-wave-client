26c21d2e4dd496e21a208323a3be86dd47a15332
faysalhosen <faysanhosen7025@gmail.com>
2023-12-01 21:08:26 +0600

    added README.md

---
+++ README.md
@@ -0,0 +1,36 @@
+# News Wave
+## A Fullstack Website
+### Technologies Used:
+#### Front End 
+* React JS
+* React Routers
+* Tailwind CSS
+* Material UI (in register, login, articles card and add pages)
+* Daisy UI (in homepage)
+* React Icons
+* Lottie React (For Animation)
+* Firebase (for authentication)
+* React Icons
+* Axios
+* Tanstack Query
+
+#### Backend
+* MongoDB
+* Express JS
+* CORS
+
+### Features of the website
+* Awesome sliders
+* User Friendly UI
+* Awesome sweet alerts
+* Admin Panel
+* Fully device responsive
+* Free Users and Premium Users Section
+* Stripe payment method system
+* Eye catching UI
+* Sticky Navbar
+* Password Authentication
+
+# Admin Panel Login Details
+* E-mail: admin@newswave.net
+* Password: Rezoan@01
--- src/pages/Features/Features.jsx
+++ src/pages/Features/Features.jsx
@@ -2,61 +2,61 @@
 const Features = () => {
     return (
         <div>
-            <section class="text-gray-600 body-font">
-                <div class="container px-5 py-10 mx-auto">
-                    <div class="text-center mb-10">
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-10 mx-auto">
+                    <div className="text-center mb-10">
                         
-                        <div class="flex mt-6 justify-center">
-                            <div class="w-16 h-1 rounded-full bg-blue inline-flex"></div>
+                        <div className="flex mt-6 justify-center">
+                            <div className="w-16 h-1 rounded-full bg-blue inline-flex"></div>
                         </div>
                     </div>
-                    <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-                        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue mb-5 flex-shrink-0">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                    <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+                        <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue mb-5 flex-shrink-0">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
                                     <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                                 </svg>
                             </div>
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Exclusive Content for Subscribers</h2>
-                                <p class="leading-relaxed text-base">Provide premium content or early access to subscribers</p>
-                                <a class="mt-3 text-blue inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <div className="flex-grow">
+                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Exclusive Content for Subscribers</h2>
+                                <p className="leading-relaxed text-base">Provide premium content or early access to subscribers</p>
+                                <a className="mt-3 text-blue inline-flex items-center">Learn More
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
                                 </a>
                             </div>
                         </div>
-                        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue mb-5 flex-shrink-0">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                        <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue mb-5 flex-shrink-0">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
                                     <circle cx="6" cy="6" r="3"></circle>
                                     <circle cx="6" cy="18" r="3"></circle>
                                     <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
                                 </svg>
                             </div>
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">User-Generated Content</h2>
-                                <p class="leading-relaxed text-base">Allow users to submit their own stories, photos, or opinions</p>
-                                <a class="mt-3 text-blue inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <div className="flex-grow">
+                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">User-Generated Content</h2>
+                                <p className="leading-relaxed text-base">Allow users to submit their own stories, photos, or opinions</p>
+                                <a className="mt-3 text-blue inline-flex items-center">Learn More
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
                                 </a>
                             </div>
                         </div>
-                        <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
-                            <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue mb-5 flex-shrink-0">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                        <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
+                            <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-blue mb-5 flex-shrink-0">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10" viewBox="0 0 24 24">
                                     <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
                                     <circle cx="12" cy="7" r="4"></circle>
                                 </svg>
                             </div>
-                            <div class="flex-grow">
-                                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">Social Media Integration</h2>
-                                <p class="leading-relaxed text-base">Facilitate easy sharing of articles on social media platforms</p>
-                                <a class="mt-3 text-blue inline-flex items-center">Learn More
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                            <div className="flex-grow">
+                                <h2 className="text-gray-900 text-lg title-font font-medium mb-3">Social Media Integration</h2>
+                                <p className="leading-relaxed text-base">Facilitate easy sharing of articles on social media platforms</p>
+                                <a className="mt-3 text-blue inline-flex items-center">Learn More
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                                     </svg>
                                 </a>
